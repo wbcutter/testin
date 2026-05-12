f1626b5ea18c3a67aaacf6b1ddc68ece433d58fa
wbcutter <plantskalan123@gmail.com>
2026-05-13 01:16:30 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,9 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/testin/",                    // ← Quan trọng nhất!
   plugins: [react(), tailwindcss(), viteSingleFile()],
-  base: '/testin/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
